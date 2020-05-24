33338856f012960a3c15bcf310adf6f1eb17b1b8
Desperado6 <1695633988@qq.com>
2020-05-25 01:08:19 +0800

    myandriod

---
+++ app/src/main/java/com/example/myapplication/Right_Fragment.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+public class Right_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.right_fragment,container,false);
+        return v;
+    }
+}
